02cb01ee7583aef698fa8020687c85d4c1faa42b
Scott McKell <scott.g.mckell@gmail.com>
2018-04-25 21:08:06 -0600

    heroku S3 attempt

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,6 +5,7 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AKIAIIXV3I4S7C45ZLRA'],
       :aws_secret_access_key => ENV['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']
+      :region                => ENV['us-west-1']
     }
     config.fog_directory     =  ENV['name-of-my-choice']
   end
